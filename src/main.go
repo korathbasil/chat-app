b4e80751b99a86801ab2af7f4b6a1bd0a9d8ffee
Bazil Korath <korathbasil@live.com>
2021-09-12 17:34:22 +0530

    add basic server with mux router

--- src/main.go
+++ src/main.go
@@ -1,5 +1,24 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func main()  {
+	fmt.Println("App Stared");
+
+	router := mux.NewRouter();
+
+	router.HandleFunc("/", homeHandler).Methods("GET");
+
+	log.Fatal(http.ListenAndServe(":8000", router))
 	
+}
+
+func homeHandler(writer http.ResponseWriter, request *http.Request) {
+	fmt.Fprintf(writer, "Hello World")
 }
