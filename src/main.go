c2561e2f46006ef32455823a2b746a71fd0b923d
Bazil Korath <korathbasil@live.com>
2021-09-12 17:39:32 +0530

    minor changes

--- src/main.go
+++ src/main.go
@@ -6,19 +6,18 @@ import (
 	"net/http"
 
 	"github.com/gorilla/mux"
-)
+);
 
-func main()  {
+func main() {
 	fmt.Println("App Stared");
 
 	router := mux.NewRouter();
 
 	router.HandleFunc("/", homeHandler).Methods("GET");
 
-	log.Fatal(http.ListenAndServe(":8000", router))
-	
+	log.Fatal(http.ListenAndServe(":8000", router));
 }
 
 func homeHandler(writer http.ResponseWriter, request *http.Request) {
-	fmt.Fprintf(writer, "Hello World")
+	fmt.Fprintf(writer, "Hello World");
 }
