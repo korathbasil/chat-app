57d252022476d250f55610dbd23578420bc75326
Bazil Korath <korathbasil@live.com>
2022-06-04 19:04:55 +0530

    chore: add pinned-chats comp file

---
+++ client/src/components/pinned-chats/PinnedChats.tsx
@@ -0,0 +1,10 @@
+import type { Component } from "solid-js";
+
+export const PinnedChats: Component = () => {
+  return (
+    <div>
+      <p>PINNED CHATS</p>
+      <div class="chats"></div>
+    </div>
+  );
+};
