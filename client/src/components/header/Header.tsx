535633dfbfc3e65384cd6cf40c18298a2e1679ac
Bazil Korath <korathbasil@live.com>
2022-06-04 18:58:26 +0530

    feat: add header
    add sass
    add icons  search, profile

--- client/src/components/header/Header.tsx
+++ client/src/components/header/Header.tsx
@@ -1,9 +1,16 @@
 import type { Component } from "solid-js";
 
+import styles from "./header.module.scss";
+import { Search, Profile } from "../../assets/icons";
+
 export const Header: Component = () => {
   return (
-    <header>
+    <header class={styles.header}>
       <h1>Ngage</h1>
+      <div class={styles.right}>
+        <Search />
+        <Profile />
+      </div>
     </header>
   );
 };
