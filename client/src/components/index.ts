7dce8f74fb13baa707e6b0a20965a5d540fbfb29
Bazil Korath <korathbasil@live.com>
2022-06-05 18:31:23 +0530

    feat: design pinned chats

--- client/src/components/index.ts
+++ client/src/components/index.ts
@@ -1,5 +1,5 @@
 export * from "./header/Header";
-export * from "./pinned-chats/PinnedChats";
+export * from "./pinned-chats/pinned=chats";
 export * from "./chat-header/ChatHeader";
 export * from "./chat-body/chat-body";
 export * from "./chat-actions/chat-actions";
