8113e46453b1ad2a255bbc34f1d52e81577027e6
Bazil Korath <korathbasil@live.com>
2022-06-05 19:13:50 +0530

    feat: add back nav from chat page

--- client/src/components/chat-header/ChatHeader.tsx
+++ client/src/components/chat-header/ChatHeader.tsx
@@ -1,13 +1,18 @@
 import type { Component } from "solid-js";
+import { Navigate, useNavigate } from "solid-app-router";
 
 import styles from "./chat-header.module.scss";
 import { BackNav, NewAudioCall, NewVideoCall } from "../../assets/icons";
 
 export const ChatHeader: Component = () => {
+  const navigate = useNavigate();
+
   return (
     <header class={styles.header}>
       <div class={styles.left}>
-        <BackNav />
+        <div onClick={() => navigate("../", { replace: true })}>
+          <BackNav />
+        </div>
         <div class={styles.profile}>
           <img
             src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cHJvZmlsZXxlbnwwfHwwfHw%3D&w=1000&q=80"
