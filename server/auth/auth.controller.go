e634529622d3264095e4f723845e7fae79121b02
Bazil Korath <korathbasil@live.com>
2022-06-08 01:28:30 +0530

    feat: add auth package

---
+++ server/auth/auth.controller.go
@@ -0,0 +1,11 @@
+package auth
+
+import "github.com/gofiber/fiber/v2"
+
+func InitializeRoutes(router fiber.Router) {
+	router.Post("/google/login", googleLoginHandler)
+}
+
+func googleLoginHandler(c *fiber.Ctx) error {
+	return c.SendString("Google login")
+}
--- server/main.go
+++ server/main.go
@@ -5,23 +5,21 @@ import (
 
 	"github.com/gofiber/fiber/v2"
 
+	"github.com/korathbasil/chat-app/server/auth"
 	"github.com/korathbasil/chat-app/server/users"
 )
 
 func main() {
-
 	app := fiber.New()
-
-	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("Hello, World!")
-	})
 
 	apiGroup := app.Group("/api")
 	v1ApiGroup := apiGroup.Group("v1")
 
 	usersRouter := v1ApiGroup.Group("/users")
-
 	users.InitializeRoutes(usersRouter)
+
+	authRouter := v1ApiGroup.Group("/auth")
+	auth.InitializeRoutes(authRouter)
 
 	log.Fatal(app.Listen(":8000"))
 }
