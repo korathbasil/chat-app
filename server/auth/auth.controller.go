afddff6ae5fd3895575e61fb1190863b57796a30
Bazil Korath <korathbasil@live.com>
2022-06-08 18:37:11 +0530

    feat [server]: add cors support

--- server/auth/auth.controller.go
+++ server/auth/auth.controller.go
@@ -1,6 +1,8 @@
 package auth
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/gofiber/fiber/v2"
+)
 
 func InitializeRoutes(router fiber.Router) {
 	router.Post("/google/login", googleLoginHandler)
--- server/main.go
+++ server/main.go
@@ -4,6 +4,7 @@ import (
 	"log"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 
 	"github.com/korathbasil/chat-app/server/auth"
 	"github.com/korathbasil/chat-app/server/users"
@@ -12,8 +13,16 @@ import (
 func main() {
 	app := fiber.New()
 
+	app.Use(cors.New())
+
+	// Or extend your config for customization
+	app.Use(cors.New(cors.Config{
+		AllowOrigins: "http://localhost:3000",
+		AllowHeaders: "Origin, Content-Type, Accept",
+	}))
+
 	apiGroup := app.Group("/api")
-	v1ApiGroup := apiGroup.Group("v1")
+	v1ApiGroup := apiGroup.Group("/v1")
 
 	usersRouter := v1ApiGroup.Group("/users")
 	users.InitializeRoutes(usersRouter)
