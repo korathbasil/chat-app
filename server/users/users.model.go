800915ca47d39108c980ffd6c4d56671e9297451
Bazil Korath <korathbasil@live.com>
2022-06-08 17:12:29 +0530

    feat: update user model

--- server/users/users.model.go
+++ server/users/users.model.go
@@ -4,8 +4,11 @@ import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type User struct {
 	Id             primitive.ObjectID `json:"id,omitempty" bson:"_id,omitempty"`
-	FullName       string             `json:"fullName,omitempty" bson:"fullName,omitempty"`
-	ProfilePicture string             `json:"profilePicture,omitempty" bson:"profilePicture,omitempty"`
+	FullName       string             `json:"fullname,omitempty" bson:"fullname,omitempty"`
+	ProfilePicture string             `json:"profilepicture,omitempty" bson:"profilepicture,omitempty"`
+	Username       string             `json:"username,omitempty" bson:"username, omitempty"`
+	Email          string             `json:"email,omitempty" bson:"email,omitempty"`
+	Phone          string             `json:"phone,omitempty" bson:"phone,omitempty"`
 }
 
 var Users = []User{}
