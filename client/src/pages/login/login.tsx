80edb51e633104346702f046a9a6e9730b47e604
Bazil Korath <korathbasil@live.com>
2022-06-08 22:47:21 +0530

    feat [client]: design login page

--- client/src/pages/login/login.tsx
+++ client/src/pages/login/login.tsx
@@ -1,15 +1,33 @@
 import { NavLink } from "solid-app-router";
 
+import { axios } from "../../config/axios";
+
 import styles from "./login.module.scss";
 import { Google } from "../../assets/icons";
 
 export const LoginPage = () => {
+  const googleLoginHandler = async () => {
+    const result = await axios.post("/auth/google/login");
+    console.log(result.data);
+  };
   return (
-    <section class={styles.main}>
-      <NavLink href="/">
-        <Google />
-        <h2>Login with Google</h2>
-      </NavLink>
-    </section>
+    <main class={styles.main}>
+      <div class={styles.child}>
+        <h1>Ngage</h1>
+
+        <h2>
+          <span>Engage</span>
+          <span>with your</span>
+          <span>loved ones</span>
+        </h2>
+
+        <div class={styles.actions}>
+          <NavLink onClick={googleLoginHandler} href="/">
+            <Google />
+            <h2>Login with Google</h2>
+          </NavLink>
+        </div>
+      </div>
+    </main>
   );
 };
