f310c66bb81a76df6c4d75d3fe858a1b464e10c7
Bazil Korath <korathbasil@live.com>
2022-06-09 05:23:29 +0530

    feat [client]: diaplay google ac info

--- client/src/pages/google-login-redirect/google-login-redirect.tsx
+++ client/src/pages/google-login-redirect/google-login-redirect.tsx
@@ -1,10 +1,12 @@
 import { createEffect } from "solid-js";
-import { useLocation } from "solid-app-router";
+import { useNavigate, useLocation } from "solid-app-router";
 
 import { axios } from "../../config/axios";
+import { setUser } from "../../data-store/user";
 
 export const GoogleLoginRedirect = () => {
   const location = useLocation();
+  const navidate = useNavigate();
   createEffect(() => {
     if (location.query.code) {
       googleLoginHandler(location.query.code);
@@ -16,7 +18,17 @@ export const GoogleLoginRedirect = () => {
       code,
     });
 
-    console.log(result.data);
+    if (result.data.verified_email) {
+      setUser(() => ({
+        id: "12121212",
+        fullName: result.data.name,
+        userName: "__ras.hi",
+        email: "rashid@email.com",
+        phone: "9875677287",
+        profilePicture: result.data.picture,
+      }));
+      navidate("/");
+    }
   };
   return <main></main>;
 };
