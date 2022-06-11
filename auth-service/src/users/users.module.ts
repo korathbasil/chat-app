631daeabf8104f0891dddf78a786665f7c1a93d9
Bazil Korath <korathbasil@live.com>
2022-06-12 01:53:15 +0530

    chore [auth]: clean dead code

--- auth-service/src/users/users.module.ts
+++ auth-service/src/users/users.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
-import { PassportModule } from '@nestjs/passport';
-import { JwtModule, JwtService } from '@nestjs/jwt';
+import { JwtService } from '@nestjs/jwt';
 
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
