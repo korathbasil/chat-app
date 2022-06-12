f4922251e1bc607501bd294af36cc5cea9f31ded
Bazil Korath <korathbasil@live.com>
2022-06-12 09:37:29 +0530

    feat [web]: add feed page file

---
+++ web/src/pages/feed/feed.tsx
@@ -0,0 +1,11 @@
+import styles from "./feed.module.scss";
+import { Header } from "../../components";
+
+export const FeedPage = () => {
+  return (
+    <main class={styles.main}>
+      <Header />
+      feed
+    </main>
+  );
+};
