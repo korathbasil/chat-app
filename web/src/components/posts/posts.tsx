823f0a5dd104647d4530151ce0742e8ba59fe3d0
Bazil Korath <korathbasil@live.com>
2022-06-13 12:12:43 +0530

    feat [web]: design post

---
+++ web/src/components/posts/posts.tsx
@@ -0,0 +1,122 @@
+import type { Component } from "solid-js";
+import { For } from "solid-js";
+import styles from "./posts.module.scss";
+
+export const Posts = () => {
+  const posts = [
+    {
+      id: "123456",
+      owner: {
+        id: "23456",
+        name: "Jessey Storms",
+        profilePicture:
+          "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cHJvZmlsZXxlbnwwfHwwfHw%3D&w=1000&q=80",
+      },
+      text: "Wow",
+      media: [
+        {
+          type: "IMG" as "IMG",
+          content:
+            "https://www.adorama.com/alc/wp-content/uploads/2018/11/landscape-photography-tips-yosemite-valley-feature.jpg",
+        },
+      ],
+      createdAt: "19:36 AM",
+    },
+    {
+      id: "123456",
+      owner: {
+        id: "23456",
+        name: "Jessey Storms",
+        profilePicture:
+          "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cHJvZmlsZXxlbnwwfHwwfHw%3D&w=1000&q=80",
+      },
+      text: "",
+      media: [
+        {
+          type: "IMG" as "IMG",
+          content:
+            "https://www.adorama.com/alc/wp-content/uploads/2018/11/landscape-photography-tips-yosemite-valley-feature.jpg",
+        },
+      ],
+      createdAt: "19:36 AM",
+    },
+  ];
+  return (
+    <section class={styles.posts}>
+      <p>FEED</p>
+      <div>
+        <For each={posts}>
+          {(post) => {
+            return <Post post={post} />;
+          }}
+        </For>
+      </div>
+    </section>
+  );
+};
+
+interface PostProps {
+  post: {
+    id: string;
+    owner: {
+      id: string;
+      name: string;
+      profilePicture: string;
+    };
+    text: string;
+    media: {
+      type: "IMG" | "VDO";
+      content: string;
+    }[];
+    createdAt: string;
+  };
+}
+
+const Post: Component<PostProps> = ({ post }) => {
+  return (
+    <div class={styles.post}>
+      <div class={styles.top}>
+        <div class={styles.left}>
+          <img src={post.owner.profilePicture} alt="" />
+          <div>
+            <h5>JJ Abrams</h5>
+            <p>jj_abrams</p>
+          </div>
+        </div>
+        <div class={styles.right}></div>
+      </div>
+      <div class={styles.content}>
+        {post.text ? <PostText text={post.text} /> : null}
+        {post.media.length > 0 ? <PostMedia media={post.media} /> : null}
+      </div>
+      <div class={styles.bootom}></div>
+    </div>
+  );
+};
+
+interface PostTextProps {
+  text: string;
+}
+
+const PostText: Component<PostTextProps> = ({ text }) => {
+  return <div class={styles.textPost}>{text}</div>;
+};
+
+interface PostMediaProps {
+  media: {
+    type: "IMG" | "VDO";
+    content: string;
+  }[];
+}
+
+const PostMedia: Component<PostMediaProps> = ({ media }) => {
+  return (
+    <div class={styles.media}>
+      <For each={media}>
+        {(item) => {
+          return <img src={item.content} />;
+        }}
+      </For>
+    </div>
+  );
+};
--- web/src/pages/feed/feed.tsx
+++ web/src/pages/feed/feed.tsx
@@ -1,11 +1,11 @@
 import styles from "./feed.module.scss";
-import { Header } from "../../components";
+import { Header, Posts } from "../../components";
 
 export const FeedPage = () => {
   return (
     <main class={styles.main}>
       <Header />
-      feed
+      <Posts />
     </main>
   );
 };
