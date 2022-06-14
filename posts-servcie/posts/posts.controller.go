4694b493a81e6dc666b9e81f429ea33b7938dfc9
Bazil Korath <korathbasil@live.com>
2022-06-14 18:47:11 +0530

    feat [posts]: add post savings into database

--- posts-servcie/posts/posts.controller.go
+++ posts-servcie/posts/posts.controller.go
@@ -1,11 +1,6 @@
 package posts
 
 import (
-	"fmt"
-	"io"
-	"os"
-	"path"
-
 	"github.com/gofiber/fiber/v2"
 
 	"github.com/korathbasil/chat-app/posts-service/utils"
@@ -16,59 +11,62 @@ func InitializeRoutes(router fiber.Router) {
 }
 
 func createPost(c *fiber.Ctx) error {
+	var (
+		isImagesAvailable = true
+		isVideosAvailable = true
+	)
+
 	images, err := utils.ParseMultipartFormData(c, "images")
 
 	if err != nil {
+		isImagesAvailable = false
+	}
+
+	imageFiles, err := utils.UploadFiles(images, "images")
+	if isImagesAvailable {
+
+		if err != nil {
+			isImagesAvailable = false
+		}
+	}
+
+	videos, err := utils.ParseMultipartFormData(c, "videos")
+
+	if err != nil {
+		isVideosAvailable = false
+	}
+
+	videosFiles, err := utils.UploadFiles(videos, "videos")
+	if isVideosAvailable {
+
+		if err != nil {
+			isVideosAvailable = false
+		}
+
+	}
+
+	if !isImagesAvailable && !isVideosAvailable {
+		if err != nil {
+			return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+				"success": false,
+				"message": "Cannot parse FormData",
+				"error":   err,
+			})
+		}
+	}
+
+	var post CraetePostDto
+
+	if err := c.BodyParser(&post); err != nil {
 		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
 			"success": false,
-			"message": "Cannot parse FormData",
+			"message": "Cannot parse JSON",
 			"error":   err,
 		})
 	}
 
-	for _, value := range images {
-		filename := path.Base(value.Filename)
-		fmt.Println(filename)
-		dest, err := os.Create("files/" + filename)
-
-		if err != nil {
-			panic(err)
-		}
-
-		defer dest.Close()
-
-		file, _ := value.Open()
-
-		res, err := io.Copy(dest, file)
-		fmt.Println(res)
-
-		if err != nil {
-			break
-		}
-
-	}
-
-	// var post CraetePostDto
-	// err := c.BodyParser(&post)
-
-	// if err != nil {
-	// 	return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
-	// 		"success": false,
-	// 		"message": "Cannot parse JSON",
-	// 		"error":   err,
-	// 	})
-	// }
-
-	// result, err := CreatePost(post)
-
-	// if err != nil {
-	// 	return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
-	// 		"success": false,
-	// 		"message": "Database operation failed",
-	// 		"error":   err,
-	// 	})
-	// }
+	result, _ := CreatePost(post, imageFiles, videosFiles)
 
 	// return c.JSON(result)
-	return c.JSON("donme")
+	return c.JSON(result)
 }
--- posts-servcie/posts/posts.service.go
+++ posts-servcie/posts/posts.service.go
@@ -2,10 +2,29 @@ package posts
 
 import "go.mongodb.org/mongo-driver/mongo"
 
-func CreatePost(post CraetePostDto) (*mongo.InsertOneResult, error) {
+func CreatePost(post CraetePostDto, imageFiles []string, videoFiles []string) (*mongo.InsertOneResult, error) {
+	var media []Media
+
+	for _, filename := range imageFiles {
+		newMedia := Media{
+			Type:    "IMG",
+			Content: filename,
+		}
+
+		media = append(media, newMedia)
+	}
+
+	for _, filename := range videoFiles {
+		newMedia := Media{
+			Type:    "VDO",
+			Content: filename,
+		}
+		media = append(media, newMedia)
+	}
+
 	newPost := Post{
 		Text:    post.Text,
-		Media:   post.Media,
+		Media:   &media,
 		Caption: post.Caption,
 		Likes:   0,
 	}
